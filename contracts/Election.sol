aa15c3097a15491dea8135bfd2edaacccf7961b7
keerthithotad <66078647+keerthithotad@users.noreply.github.com>
2020-06-07 08:00:19 +0530

    Add files via upload

---
+++ contracts/Election.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+//model candidate
+     struct Candidate{
+ 		 	uint id;
+  			string name;
+  			uint voteCount;
+					 }
+//store a candidate
+
+// store accounts that have voted
+ mapping(address => bool) public voters;
+
+ function vote(uint _candidateId) public {
+
+   // require that the voter hasn't voted before
+  require (!voters[msg.sender]);
+
+  // require a valid candidate
+  require (_candidateId > 0 && _candidateId <= candidatesCount);
+
+  //record  that voter has voted
+  voters[msg.sender] = true;
+
+// update candiadte voteCount
+  candidates[_candidateId].voteCount ++;
+  }
+
+//fetch Candidate
+ mapping(uint => Candidate) public candidates;
+
+ //store candidate count
+
+ uint public candidatesCount;
+
+ //constructor
+     constructor () public {
+
+        addCandidate("Candidate 1");
+
+        addCandidate("Candidate 2");
+
+    }
+
+  function addCandidate (string memory _name) private {
+  candidatesCount ++;
+  candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+ } 
+
+
+}
+   
